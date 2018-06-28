830f141bc44488d615615c21508c4633e466b2be
sekisuri <sekisuri@gmail.com>
2018-06-29 07:46:25 +0900

    리지드바디를 이용한 캐릭터 이동

---
+++ Assets/Scripts/Ch01/Movement04.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 리지드바디를 이용한 캐릭터 이동
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class Movement04 : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private Rigidbody rb;
+    private Vector3 moveDir;
+    // private Transform tr;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        //tr = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        moveDir.Set(h, 0, v);
+        moveDir = moveDir.normalized * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + moveDir);
+
+    }
+}
+
+   
