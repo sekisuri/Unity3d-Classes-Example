685b80db295bd3f93a0de9f3ca28df6393f9237e
sekisuri <sekisuri@gmail.com>
2018-06-30 17:46:36 +0900

    주석추가

--- Assets/Scripts/Etc/Enemy.cs
+++ Assets/Scripts/Etc/Enemy.cs
@@ -16,6 +16,10 @@ public class Enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// NavmeshAgent의 destination에 어떤 위치를 설정하면 해당 위치로 자동으로 이동된다.
+		// 이번절에서는 사용자가 Inspector에서 게임 오브젝트를 지정하는데,
+		// 지정한 게임 오브젝트의 위치인 target.transform.position을 NavMeshAgent의
+		// destination에 설정하는 코드다.
 		agent.destination = target.transform.position;
 		animator.SetFloat ("Speed", agent.velocity.magnitude);
 	
